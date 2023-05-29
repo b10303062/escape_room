30f70f113a3230fe7d761f051605c111089d4843
lmj1119 <91189627+lmj1119@users.noreply.github.com>
2023-05-29 21:21:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+WIN_WIDTH = 1000
+WIN_HEIGHT = 700
+TILESIZE = 50
+FPS = 60
+
+PLAYER_LAYER = 2
+BLOCK_LAYER = 1
+
+PLAYER_SPEED = 3
+
+RED = (255, 0, 0)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBBBBB',
+    'B.....................B',
+    'B...............BBBBBBB',
+    'B...............B.....B',
+    'B.....................B',
+    'B.............BBBBBBBBB',
+    'B.....................B',
+    'B...........P.........B',
+    'B.....................B',
+    'B.....................B',
+    'B.....................B',
+    'B.....................B',
+    'B.....................B',
+    'BBBBBBBBBBBBBBBBBBBBBBB',
+]
--- main.py
+++ main.py
@@ -1,46 +1,68 @@
-import pygame as pg
-import game
-from game.engine import Engine
-from game.room import Room
-from game.item import Item
-from game.exit import Exit
-import random
+import pygame
+from sprites import *
+from config import *
+import sys
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def createTilemap(self):
+        for i,row in enumerate(tilemap):
+            for j,column in enumerate(row):
+                if column == "B":
+                    Block(self,j,i)
+                if column == "P":
+                    Player(self,j,i)
+
+    def new(self):
+        self.palying = True
+
+        self.all_sprites = pygame.sprite.LayeredUpdates()
+        self.blocks = pygame.sprite.LayeredUpdates()
+        self.enemies = pygame.sprite.LayeredUpdates()
+        self.attacks = pygame.sprite.LayeredUpdates()
+
+        self.createTilemap()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.palying = False
+                self.running = False
+
+    def update(self):
+        self.all_sprites.update()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        self.clock.tick(FPS)
+        pygame.display.update()
 
 
-def generate_room():
-    a_exit = Exit()
-    a_room = Room(a_exit)
-    for i in range(random.randrange(1, 10)):
-        a_item = Item()
-        a_room.add_item(a_item)
+    def main(self):
+        while self.palying:
+            self.events()
+            self.update()
+            self.draw()
+        self.running = False
 
-    return a_room
+    def game_over(self):
+        pass
 
+    def intro_screen(self):
+        pass
 
-# initialize the game
-first_room = generate_room()
-game_engine = Engine(first_room)
-game_engine.set_active_sprites()
+g = Game()
+g.intro_screen()
+g.new()
+while g.running:
+    g.main()
+    g.game_over()
 
-# game loop
-game_on = True
-while game_on is True:
-
-    # Process player inputs
-    for event in pg.event.get():
-         if event.type == pg.QUIT:
-             game_on = False
-
-    # Do logical updates here.
-    game_engine.active_sprites.update()
-
-    # Fill the display with a solid color
-    game_engine.screen.fill(game.SOLID_COLOR)
-
-    # Render the graphics here.
-    game_engine.active_sprites.draw(game_engine.screen)
-
-    pg.display.flip()  # Refresh on-screen display
-    game_engine.clock.tick(game.FPS)  # wait until next frame (at 60 FPS)
-
-pg.quit()
+pygame.quit()
+sys.exit()
